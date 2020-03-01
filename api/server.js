0dad14e6c247a865551aa5965ae3033e7b71ae53
Mark Allison <mark.allison75@gmail.com>
2020-03-01 09:13:38 -0500

    start

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+
+
+server.get("/", (req, res) => {
+    res.json({message: "Do Or Do Not There Is No Try!"})
+})
+
+module.exports = server;
